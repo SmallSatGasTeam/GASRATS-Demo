ed04839dbc234eb54b25bbe34360b20209288aff
Devin Schutz <schutzdevin@gmail.com>
2024-06-24 12:12:00 -0600

    Accidentally had the wrong gds deployment

--- FirstTimeCompile32.sh
+++ FirstTimeCompile32.sh
@@ -24,4 +24,4 @@ fprime-util build arm-hf-linux -j $(nproc)
 scp -r build-artifacts/arm-hf-linux/FSWDeployment gas@pi0.gas.usu.edu:deployment
 
 # starts the gds on this machine
-fprime-gds -n --dictionary build-artifacts/aarch64-linux/FSWDeployment/dict/FSWDeploymentTopologyAppDictionary.xml --ip-client --ip-address pi0.gas.usu.edu
+fprime-gds -n --dictionary build-artifacts/arm-hf-linux/FSWDeployment/dict/FSWDeploymentTopologyAppDictionary.xml --ip-client --ip-address pi0.gas.usu.edu
