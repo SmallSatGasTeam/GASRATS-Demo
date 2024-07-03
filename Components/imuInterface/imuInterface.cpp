90df24509d29324432b45bb91dd58c2e6ab73ae0
Devin Schutz <schutzdevin@gmail.com>
2024-07-03 12:13:13 -0600

    made minor changes to the cross-compile scripts, and for whatever reason the instances.fpp file changed TcpClient back to TcpServer so I reverted that but it's still not connecting so idk what that's about.

--- Components/imuInterface/imuInterface.cpp
+++ Components/imuInterface/imuInterface.cpp
@@ -41,7 +41,7 @@ namespace Components {
 
     #ifndef VIRTUAL
       // the number '0011110' is the slave address for the Magnetorqer registers
-      this->requestI2CData_out(0, 0011110, imuData); // this will update the buffer 'imuData' with the data from the slave device
+      this->requestI2CData_out(0, 0x6B, imuData); // this will update the buffer 'imuData' with the data from the slave device
       this->collector_out(0, 1234); // this is supposted to output the buffer data but idk how to make an output with type 'Buffer'
     #endif
 
