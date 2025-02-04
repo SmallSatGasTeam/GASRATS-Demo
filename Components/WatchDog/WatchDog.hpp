7b2a30ae24c7f0f8ab7b24baebbab1fb4e4e8e66
Hyrum Smith <hyrum.d.smith@gmail.com>
2025-02-03 19:13:43 -0700

    Added functionality for catching asserts. Need to test still

--- Components/WatchDog/WatchDog.hpp
+++ Components/WatchDog/WatchDog.hpp
@@ -11,32 +11,68 @@
 
 namespace Components {
 
+// Global variable to communicate between asserts and the WatchDog component
+extern bool healthy;
+
 class WatchDog : public WatchDogComponentBase {
   public:
-    // ----------------------------------------------------------------------
-    // Component construction and destruction
-    // ----------------------------------------------------------------------
+      int numPings;
+      // ----------------------------------------------------------------------
+      // Component construction and destruction
+      // ----------------------------------------------------------------------
 
-    //! Construct WatchDog object
-    WatchDog(const char* const compName  //!< The component name
-    );
+      //! Construct WatchDog object
+      WatchDog(const char *const compName //!< The component name
+      );
 
-    //! Destroy WatchDog object
-    ~WatchDog();
+      //! Destroy WatchDog object
+      ~WatchDog();
 
-  PRIVATE:
-    // ----------------------------------------------------------------------
-    // Handler implementations for user-defined typed input ports
-    // ----------------------------------------------------------------------
+      PRIVATE :
+          // ----------------------------------------------------------------------
+          // Handler implementations for user-defined typed input ports
+          // ----------------------------------------------------------------------
 
-    //! Handler implementation for healthIn
-    //!
-    //! Accepts the code coming in from the health component
-    void healthIn_handler(NATIVE_INT_TYPE portNum,  //!< The port number
-                          U32 code                  //!< Watchdog stroke code
-                          ) override;
+          //! Handler implementation for healthIn
+          //!
+          //! Accepts the code coming in from the health component
+          void
+          healthIn_handler(NATIVE_INT_TYPE portNum, //!< The port number
+                           U32 code                 //!< Watchdog stroke code
+                           ) override;
 
-    bool stroke;
+      bool stroke;
+};
+
+class GaspacsAssertHook : public Fw::AssertHook {
+  public:
+    GaspacsAssertHook() : previousHook(nullptr) {
+    }
+
+    ~GaspacsAssertHook() {
+    }
+
+    void reportAssert(FILE_NAME_ARG file, NATIVE_UINT_TYPE lineNo, NATIVE_UINT_TYPE numArgs, FwAssertArgType arg1,
+                      FwAssertArgType arg2, FwAssertArgType arg3, FwAssertArgType arg4, FwAssertArgType arg5,
+                      FwAssertArgType arg6) override;
+
+    void doAssert() override;
+
+    void defaultReportAssert(
+        FILE_NAME_ARG file,
+        NATIVE_UINT_TYPE lineNo,
+        NATIVE_UINT_TYPE numArgs,
+        FwAssertArgType arg1,
+        FwAssertArgType arg2,
+        FwAssertArgType arg3,
+        FwAssertArgType arg4,
+        FwAssertArgType arg5,
+        FwAssertArgType arg6,
+        CHAR *destBuffer,
+        NATIVE_INT_TYPE buffSize);
+
+private:
+    Fw::AssertHook* previousHook;
 };
 
 }  // namespace Components
