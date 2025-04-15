7695b923709c7419fa8416bb1ce10efe355bfe0e
Devin Schutz <schutzdevin@gmail.com>
2025-04-14 19:28:28 -0600

    Removed the broken part of the project.cmake. Updated to fprime v3.6.2, and added a comment to my bash file that copies the custom framer into the fprime repo

---
+++ CustomFramerProtocol/copyToFprimeFolder.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cp EndurosatProtocol.* ../fprime/Svc/FramingProtocol
+# THIS MUST BE RAN IN THE FOLDER IT IS IN
--- CutsomFramerProtocol/copyToFprimeFolder.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-cp EndurosatProtocol.* ../fprime/Svc/FramingProtocol
